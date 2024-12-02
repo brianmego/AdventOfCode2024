22d247cdd0f147a35ea6bbf04399e4b538f06a62
Brian Mego <brian.mego@q2.com>
2024-12-02 13:25:08 -0600

    WIP

--- day-2/src/bin/part2.rs
+++ day-2/src/bin/part2.rs
@@ -1,5 +1,23 @@
 mod shared;
+use shared::{InputList, Report, PUZZLE_INPUT};
 
 fn main() {
+    let reports = InputList(PUZZLE_INPUT).get_reports();
+    println!("{}", get_safe_count(reports));
+}
 
+fn get_safe_count(reports: Vec<Report>) -> usize {
+    reports.iter().filter(|r| r.is_safe(1)).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_safe_reports() {
+        let reports = InputList(PUZZLE_INPUT).get_reports();
+        let actual = get_safe_count(reports);
+        assert_eq!(actual, 4);
+    }
 }
--- day-2/src/bin/shared/mod.rs
+++ day-2/src/bin/shared/mod.rs
@@ -11,21 +11,83 @@ impl<'a> InputList<'a> {
         Self(inp)
     }
     pub fn get_reports(&self) -> Vec<Report> {
-        self.0.lines().into_iter().map(|x| {
-            Report(
-                x.split_whitespace()
-                    .into_iter()
-                    .map(|level| level.parse().expect("known input")).collect(),
-            )
-        }).collect()
+        self.0
+            .lines()
+            .into_iter()
+            .map(|x| {
+                Report(
+                    x.split_whitespace()
+                        .into_iter()
+                        .map(|level| level.parse().expect("known input"))
+                        .collect(),
+                )
+            })
+            .collect()
+    }
+}
+#[derive(PartialEq, Eq, Debug)]
+enum AccelerationType {
+    Increasing,
+    Decreasing,
+    Same,
+}
+#[derive(Debug)]
+struct Movement {
+    safe: bool,
+    direction: AccelerationType,
+}
+
+impl Movement {
+    fn new(safe: bool, direction: AccelerationType) -> Self {
+        Self { safe, direction }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Report(Vec<usize>);
 impl Report {
-    pub fn get_levels(&self) -> Vec<usize> {
+    fn get_levels(&self) -> Vec<usize> {
         self.0.clone()
+    }
+    pub fn is_safe(&self, tolerance: u8) -> bool {
+        let levels = self.get_levels();
+        let mut prev_level = levels[0];
+        let mut movements: Vec<Movement> = vec![];
+        let mut unsafe_count = 0;
+        for level in levels[1..].iter() {
+            let safe = level.abs_diff(prev_level) <= 3;
+            let movement = match level.cmp(&prev_level) {
+                std::cmp::Ordering::Less => Movement::new(safe, AccelerationType::Decreasing),
+                std::cmp::Ordering::Equal => Movement::new(false, AccelerationType::Same),
+                std::cmp::Ordering::Greater => Movement::new(safe, AccelerationType::Increasing),
+            };
+            match movement.safe {
+                true => {
+                    let prev_acceleration = movements.last();
+                    let is_safe = match prev_acceleration {
+                        Some(last) => {
+                            if last.direction != movement.direction {
+                                unsafe_count += 1;
+                                false
+                            } else {
+                                true
+                            }
+                        }
+                        None => true,
+                    };
+                    if is_safe == true {
+                        prev_level = *level;
+                        movements.push(movement);
+                    }
+                }
+                false => unsafe_count += 1,
+            }
+            if unsafe_count > tolerance {
+                return false;
+            }
+        }
+        dbg!(movements);
+        true
     }
 }
 
@@ -43,8 +105,31 @@ mod tests {
             Report(vec![9, 7, 6, 2, 1]),
             Report(vec![1, 3, 2, 4, 5]),
             Report(vec![8, 6, 4, 4, 1]),
-            Report(vec![1, 3, 6, 7, 9])
+            Report(vec![1, 3, 6, 7, 9]),
         ];
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_is_safe_report() {
+        let reports = InputList(PUZZLE_INPUT).get_reports();
+        assert_eq!(reports[0].is_safe(0), true);
+        assert_eq!(reports[1].is_safe(0), false);
+        assert_eq!(reports[2].is_safe(0), false);
+        assert_eq!(reports[3].is_safe(0), false);
+        assert_eq!(reports[4].is_safe(0), false);
+        assert_eq!(reports[5].is_safe(0), true);
+    }
+
+    #[test]
+    fn test_is_safe_report_tolerance_1() {
+        let reports = InputList(PUZZLE_INPUT).get_reports();
+        assert_eq!(reports[0].is_safe(1), true);
+        assert_eq!(reports[1].is_safe(1), false);
+        assert_eq!(reports[2].is_safe(1), false);
+        assert_eq!(reports[3].is_safe(1), true);
+        assert_eq!(reports[4].is_safe(1), true);
+        assert_eq!(reports[5].is_safe(1), true);
+        assert!(false);
     }
 }
